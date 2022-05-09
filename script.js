ab0dae51a8b47415f3e2e1d8bde46a3a9cd3e62d
aeh1707 <abdelhadi.hireche@outlook.com>
2022-05-09 12:37:20 +0100

    Add styles to the mobile menu

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector("nav ul");
 const logo = document.querySelector(".logo");
+const x = document.querySelector(".x");
 burger.addEventListener("click", function () {
   menu.classList.add("mobile-view");
   burger.classList.add("burger-none");
   logo.classList.add("logo-none");
+  x.classList.add("x-appear");
 });
